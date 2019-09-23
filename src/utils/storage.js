ee6d463bd37eafc06464c51dceb0b6a7ffa1d48a
qinmor <15821370082@163.com>
2019-09-23 15:34:41 +0800

    3

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -4,11 +4,12 @@
 export default {
   put: function(key, value) {
     window.localStorage.setItem(key, value);
+    
   },
 
   get: function(key) {
     return window.localStorage.getItem(key);
-    
+
   },
 
   remove: function(key) {
